7a113765404ddb5db5e3df7375119dad8d13d5ff
Pajehome <abdel20069@gmail.com>
2022-05-02 16:37:30 +0200

    mail

--- main.js
+++ main.js
@@ -2,23 +2,18 @@ const app = new Vue({
     el: '#app',
     data: {
         mail:[],
+        dati: 10,
     },
     methods: {},
     created(){
       
-      for(let i = 0; i<10; i++){
+      for(let i = 0; i<this.dati; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
           this.mail.push(resp.data.response) 
+        }).catch((err)=>{
+            console.error(err)
         })
       }
 
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-        this.mail = resp.data.response;
-      }).catch((err)=>{
-          console.error(err)
-      })  
-
-      
-       
     }
 })
